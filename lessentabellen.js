4b63614af50baa214939a37ede60dc691cdf1032
GOcampusRedingenhof <gocampusredingenhof@gmail.com>
2025-04-15 14:50:32 +0200

    Update lessentabellen.js

--- lessentabellen.js
+++ lessentabellen.js
@@ -1,1 +1,14 @@
+/**
+ * Dynamische Lessentabellen - Main CSS File
+ * Importeert alle CSS modules
+ * @version 2.2.0
+ * @copyright 2025 GO Campus Redingenhof
+ */
 
+/* Import alle modules */
+@import url('lessentabellen-core.css');
+@import url('lessentabellen-grid.css');
+@import url('lessentabellen-slidein.css');
+@import url('lessentabellen-table.css');
+@import url('lessentabellen-print.css');
+@import url('lessentabellen-responsive.css');
