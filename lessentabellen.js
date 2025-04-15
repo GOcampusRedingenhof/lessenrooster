a896897859811c85f4025fee3626eb11694b1b11
GOcampusRedingenhof <gocampusredingenhof@gmail.com>
2025-04-15 14:56:32 +0200

    lessentabellen.js

--- lessentabellen.js
+++ lessentabellen.js
@@ -1,14 +1,85 @@
 /**
- * Dynamische Lessentabellen - Main CSS File
- * Importeert alle CSS modules
+ * Dynamische Lessentabellen - Hoofdbestand
+ * Initialiseert en verbindt alle modules
  * @version 2.2.0
  * @copyright 2025 GO Campus Redingenhof
  */
 
-/* Import alle modules */
-@import url('lessentabellen-core.css');
-@import url('lessentabellen-grid.css');
-@import url('lessentabellen-slidein.css');
-@import url('lessentabellen-table.css');
-@import url('lessentabellen-print.css');
-@import url('lessentabellen-responsive.css');
+// Gebruik een IIFE (Immediately Invoked Function Expression) om de code te isoleren
+(function() {
+  'use strict';
+  
+  // Controleer of de app al is gedefinieerd om dubbele instantiatie te voorkomen
+  if (window.LessentabellenApp) {
+    console.warn('Lessentabellen app is al gedefinieerd. Tweede instantiatie overgeslagen.');
+    return;
+  }
+  
+  // Applicatie object - Toegankelijk via window.LessentabellenApp
+  const LessentabellenApp = {
+    // Import modules
+    config: LessentabellenConfig,
+    data: LessentabellenData,
+    ui: LessentabellenUI,
+    utils: LessentabellenUtils,
+    events: LessentabellenEvents,
+    
+    // Initialisatie
+    init: function(skipElementCreation) {
+      console.log(`Lessentabellen v${this.config.getVersion()} initializing...`);
+      
+      // Detecteer mobiel
+      this.data.setData('isMobile', window.innerWidth <= 768);
+      
+      // Alleen DOM elementen aanmaken als dat nodig is
+      if (!skipElementCreation) {
+        this.createRequiredElements();
+      }
+      
+      // Initialiseer modules en verbind ze
+      this.initializeModules();
+      
+      // Laad data
+      this.data.loadData();
+      
+      // Verbeterde detectie van tophoogte met meerdere pogingen
+      this.utils.setDynamicTop();
+      setTimeout(() => this.utils.setDynamicTop(), 500);
+      setTimeout(() => this.utils.setDynamicTop(), 1500);
+    },
+    
+    // Modules initialiseren en verbinden
+    initializeModules: function() {
+      // Initialiseer modules met dependencies
+      this.utils.init(this.config, this.data, this.ui);
+      this.data.init(this.config, this.utils, this.ui);
+      this.ui.init(this.config, this.data, this.utils);
+      this.events.init(this.config, this.data, this.ui, this.utils);
+    },
+    
+    // DOM elementen creëren indien nodig
+    createRequiredElements: function() {
+      console.log("Checking if elements exist...");
+      
+      // Toon een waarschuwing als er al elementen bestaan met de naam zonder prefix
+      const oldContainer = document.getElementById('lessentabellen-container');
+      if (oldContainer) {
+        console.warn('Een oude versie van lessentabellen-container is gevonden. Dit kan conflicten veroorzaken.');
+      }
+      
+      const prefix = this.config.getConfig().prefixClass.slice(0, -1);
+      
+      // Als main container niet bestaat, maak deze aan
+      if (!document.getElementById(`${prefix}-container`)) {
+        console.log("Creating main container");
+        const mainContainer = document.createElement('div');
+        mainContainer.id = `${prefix}-container`;
+        mainContainer.className = `${prefix}-root`;
+        document.body.appendChild(mainContainer);
+        
+        // Voeg interne elementen toe
+        mainContainer.innerHTML = `
+          <div id="${prefix}-domains-container"></div>
+          <div id="${prefix}-overlay"></div>
+          <div class="${prefix}-wrapper" id="${prefix}-slidein" tabindex="-1">
+            <button class="${prefix}-close-btn"/**
